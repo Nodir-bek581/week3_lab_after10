6905121c245da6c6757544a9a3577b55bf1f9dc2
Nodir-bek581 <nodirbekodilov581@gmail.com>
2025-09-10 12:30:14 +0500

    problems 11-20

---
+++ main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+//Problem11
+/*int main() {
+    double x = 1.13, y = 411;
+    cout << x*10*10*10 << " " << y/(10*10*10*10) << endl;
+}*/
+
+//Problem12
+/*int main() {
+    int numberStudents = 241;
+    cout << "In first group: " << numberStudents/11 << endl;
+    cout << "In last group; " << numberStudents%11 << endl;
+}*/
+
+//Problem13
+/*int main() {
+    float side;
+    cin >> side;
+    float area = (3*1.73*side*side)/2;
+    cout << area << endl;
+}*/
+
+//Problem14
+/*int main() {
+    int num = 31;
+    num = num%9;
+    int num1 = ++num;
+    int num2 = num*3;
+    int num3 = num - 14;
+    int num4 = num/4;
+    int num5 = --num;
+    cout << num << endl;
+}*/
+
+//Problem15
+/*int main() {
+    char a = 'A';
+    cout << char(a + 7) << char(a + 4) << char(a + 11) << char(a + 11) << char(a + 14) << char(a - 32) << endl;
+}*/
+
+//Problem16
+/*int main() {
+    float x=17;
+    float y=1;
+    float z=(x+(++x))/(((++x)/6)*(++y));
+    cout << z << endl;
+}*/
+
+//Problem17
+/*int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a==b) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}*/
+
+//problem18
+/*int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a<140 and a<b) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}*/
+
+//problem19
+/*int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if (a>b and a>c) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}*/
+
+//Problem20
+/*int main() {
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    if (a>=0 or b>=0 or c>=0 or d>=0 or e>=0) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+}*/
